d3c50a6f3c3aff03e4c94f02832fb84a156aa98c
daosgava <daosgava.garcia@gmail.com>
2024-09-21 20:26:41 +1000

    adding CI/CD scripts

--- scripts/delete.sh
+++ scripts/delete.sh
@@ -1,1 +1,15 @@
-# Your Solution
+# 
+# Remove containers from Kubernetes.
+#
+# Environment variables:
+#
+#   NAME - The name of the microservice to delete.
+#
+# Usage:
+#
+#   ./scripts/delete.sh
+#
+set -u # or set -o nounset
+: "$NAME"
+
+envsubst < ./scripts/kubernetes/${NAME}.yaml | kubectl delete -f -
